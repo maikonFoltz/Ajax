15becdc55d15745a6f56da92e38dfb14c70d1cf1
maikonFoltz <maikonjfoltz@gmail.com>
2020-11-03 21:49:55 -0300

    ok

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ajax</title>
+    <style>
+    .div{
+        width: 200px;
+        height: 170px;
+        background-color: #EEE;
+    }
+    
+    </style>
+</head>
+<body>
+    <div class="div">
+    </div>
+    <button>Ação</button>
+
+    <script src="script.js"></script>
+</body>
+</html>
